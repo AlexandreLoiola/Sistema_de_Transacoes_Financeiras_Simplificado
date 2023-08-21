548afd03e6beb94d37af11c82a168deba0f89228
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-08-20 22:19:21 -0300

    feat: initial config

---
+++ src/main/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/TransferenciaFinanceiraSimplificadaApplication.java
@@ -0,0 +1,12 @@
+package com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciaFinanceiraSimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciaFinanceiraSimplificadaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/TransferenciaFinanceiraSimplificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciaFinanceiraSimplificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
