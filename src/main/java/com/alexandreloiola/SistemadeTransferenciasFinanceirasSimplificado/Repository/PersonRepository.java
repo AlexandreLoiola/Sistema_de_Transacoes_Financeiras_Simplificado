2220c7f298bca4c0f14618bada98b567297d5c14
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-08-21 21:25:01 -0300

    feat: Adiciona o arquivo PersonRepository

---
+++ src/main/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/Repository/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado.Repository;
+
+import com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado.Model.PersonModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<PersonModel, Long> {
+    Optional<PersonModel> findByDocument(String document);
+
+}
