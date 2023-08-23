602bd86526aa3772537aa1545be82edca8159bea
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-08-22 23:19:00 -0300

    feat: Adiciona o arquivo PersonDto

---
+++ src/main/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/Rest/Dto/PersonDto.java
@@ -0,0 +1,14 @@
+package com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado.Rest.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDto {
+
+    private String name;
+    private String document;
+}
