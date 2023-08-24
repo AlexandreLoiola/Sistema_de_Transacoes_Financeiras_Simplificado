49498ef78da45074626de52e4edd751e078477b4
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-08-24 10:38:54 -0300

    refact: Validações RoleForm

--- src/main/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/Rest/Form/RoleForm.java
+++ src/main/java/com/alexandreloiola/SistemadeTransferenciasFinanceirasSimplificado/Rest/Form/RoleForm.java
@@ -2,12 +2,17 @@ package com.alexandreloiola.SistemadeTransferenciasFinanceirasSimplificado.Rest.
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class RoleForm {
 
     @NotNull(message = "O campo descrição não pode estar vazio")
+    @NotBlank(message = "O campo nome não pode ficar em branco.")
+    @Size(min = 3, max = 100, message = "A descrição do papel de usuário deve ter entre 3 e 100 caracteres.")
     private String description;
 
 }
